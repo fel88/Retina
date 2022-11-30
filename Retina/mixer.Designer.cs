deb75c423dacb3e81dfdaf39ea5538592f90ead6
fel88 <15663687+fel88@users.noreply.github.com>
2022-11-30 15:04:00 +0300

    upd

--- Retina/mixer.Designer.cs
+++ Retina/mixer.Designer.cs
@@ -38,16 +38,22 @@
             this.checkBox1 = new System.Windows.Forms.CheckBox();
             this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
             this.panel1 = new System.Windows.Forms.Panel();
+            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown3 = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
             this.statusStrip1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.tableLayoutPanel1.SuspendLayout();
             this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown3)).BeginInit();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(3, 18);
+            this.button1.Location = new System.Drawing.Point(7, 18);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 0;
@@ -65,7 +71,7 @@
             // 
             // numericUpDown1
             // 
-            this.numericUpDown1.Location = new System.Drawing.Point(359, 21);
+            this.numericUpDown1.Location = new System.Drawing.Point(382, 23);
             this.numericUpDown1.Maximum = new decimal(new int[] {
             100000,
             0,
@@ -120,7 +126,7 @@
             // checkBox1
             // 
             this.checkBox1.AutoSize = true;
-            this.checkBox1.Location = new System.Drawing.Point(271, 23);
+            this.checkBox1.Location = new System.Drawing.Point(364, 6);
             this.checkBox1.Name = "checkBox1";
             this.checkBox1.Size = new System.Drawing.Size(82, 17);
             this.checkBox1.TabIndex = 6;
@@ -145,6 +151,10 @@
             // 
             // panel1
             // 
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.numericUpDown3);
+            this.panel1.Controls.Add(this.numericUpDown2);
             this.panel1.Controls.Add(this.button1);
             this.panel1.Controls.Add(this.checkBox1);
             this.panel1.Controls.Add(this.textBox1);
@@ -155,6 +165,45 @@
             this.panel1.Name = "panel1";
             this.panel1.Size = new System.Drawing.Size(635, 54);
             this.panel1.TabIndex = 5;
+            // 
+            // numericUpDown2
+            // 
+            this.numericUpDown2.Location = new System.Drawing.Point(455, 23);
+            this.numericUpDown2.Name = "numericUpDown2";
+            this.numericUpDown2.Size = new System.Drawing.Size(54, 20);
+            this.numericUpDown2.TabIndex = 7;
+            this.numericUpDown2.ValueChanged += new System.EventHandler(this.numericUpDown2_ValueChanged);
+            // 
+            // numericUpDown3
+            // 
+            this.numericUpDown3.Location = new System.Drawing.Point(515, 23);
+            this.numericUpDown3.Name = "numericUpDown3";
+            this.numericUpDown3.Size = new System.Drawing.Size(54, 20);
+            this.numericUpDown3.TabIndex = 8;
+            this.numericUpDown3.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown3.ValueChanged += new System.EventHandler(this.numericUpDown3_ValueChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(452, 7);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(33, 13);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "cam1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(512, 7);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(33, 13);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "cam2";
             // 
             // mixer
             // 
@@ -172,6 +221,8 @@
             this.tableLayoutPanel1.ResumeLayout(false);
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown3)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -189,5 +240,9 @@
         private System.Windows.Forms.CheckBox checkBox1;
         private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
         private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NumericUpDown numericUpDown3;
+        private System.Windows.Forms.NumericUpDown numericUpDown2;
     }
 }
--- Retina/mixer.cs
+++ Retina/mixer.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -21,17 +22,48 @@ namespace Retina
 
         private async void button1_Click(object sender, EventArgs e)
         {
+            int ofps = 30;
+            int ww = 1920;
+            int hh = 1080;
+            var fn = textBox1.Text;
+            if (File.Exists(fn))
+            {
+                var res = MessageBox.Show($"File {fn} exists. Overwrite?", Text, MessageBoxButtons.YesNoCancel, MessageBoxIcon.Question);
+                switch (res)
+                {
+                    case DialogResult.Yes:
+                        break;
+                    case DialogResult.No:
+                        int ii = 0;
+                        while (true)
+                        {
+                            var nfn = Path.GetFileNameWithoutExtension(fn) + "_" + ii + Path.GetExtension(fn);
+                            if (!File.Exists(nfn))
+                            {
+                                fn = nfn; 
+                                break;
+                            }
+                            ii++;
+                            if (ii > 1000) return;
+
+                        }
+
+                        break;
+                    case DialogResult.Cancel:
+                        return;
+                }
+            }
             button1.Enabled = false;
+
+            var out_vid = new VideoWriter(fn, FourCC.XVID, ofps < 0.5 ? 30 : ofps,
+                new OpenCvSharp.Size(ww * 2, hh));
+
             await Task.Run(() =>
             {
-                int ofps = 30;
-                int ww = 1920;
-                int hh = 1080;
-                var out_vid = new VideoWriter(textBox1.Text, FourCC.XVID, ofps < 0.5 ? 30 : ofps,
-                    new OpenCvSharp.Size(ww * 2, hh));
 
-                var v1 = openCam(0, true, false, ww, hh);
-                var v2 = openCam(1, true, false, ww, hh);
+
+                var v1 = openCam(cam1Idx, true, false, ww, hh);
+                var v2 = openCam(cam2Idx, true, false, ww, hh);
                 int cnt = (int)numericUpDown1.Value;
                 int i = 0;
                 //for (int i = 0; i < cnt; i++)
@@ -42,7 +74,7 @@ namespace Retina
                     if (fixedFrames)
                     {
                         if (i >= cnt) break;
-                      
+
                     }
                     else if (stop)
                         break;
@@ -87,10 +119,24 @@ namespace Retina
         {
             stop = true;
         }
+
         bool fixedFrames = false;
+
         private void checkBox1_CheckedChanged(object sender, EventArgs e)
         {
-            fixedFrames = true;
+            fixedFrames = checkBox1.Checked;
+        }
+
+        int cam1Idx = 0;
+        int cam2Idx = 0;
+        private void numericUpDown2_ValueChanged(object sender, EventArgs e)
+        {
+            cam1Idx = (int)numericUpDown2.Value;
+        }
+
+        private void numericUpDown3_ValueChanged(object sender, EventArgs e)
+        {
+            cam2Idx = (int)numericUpDown3.Value;
         }
     }
 }
